3c9219cf9f83eecccbda47726ea1be1ecd448461
narantyo maulana <olandeoxys@gmail.com>
2022-11-01 10:34:57 +0700

    Fix Download CV and  index.html

--- js/app.js
+++ js/app.js
@@ -1,11 +1,4 @@
 $(document).ready(function(){
-$('.slider').slick({
-    arrows:false,
-    dots:true,
-    appendDots:'.slider-dots',
-    dotsClass:'dots'
-});
-
 
 let hamberger = document.querySelector('.hamberger');
 let times = document.querySelector('.times');
